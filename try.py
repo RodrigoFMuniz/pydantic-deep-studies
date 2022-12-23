f9b4b97361c9908d4628d76fc6ca918dcffc37ba
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-12-23 16:38:05 -0300

    Using Try block

---
+++ try.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ValidationError
+
+class Person(BaseModel):
+    first_name:str
+    last_name:str
+    age:int
+
+
+if __name__=="__main__":
+
+    try:
+        p1 = Person(first_name="Foo", last_name="Boo", age="error")
+    
+    except ValidationError as err:
+        print(err)
+
